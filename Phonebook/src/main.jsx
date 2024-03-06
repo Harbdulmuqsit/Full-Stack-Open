003fcdcd9563406b60518a50f252b8d950356818
Abdulsalam Abdulmuqsit Olamide <harbdulsarlam@gmail.com>
2024-03-06 17:35:56 +0100

    Fresh start second commit

--- Phonebook/src/main.jsx
+++ Phonebook/src/main.jsx
@@ -1,4 +1,18 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import axios from 'axios'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
+
+ axios.get('http://localhost:3001/notes').then(rest => { const notes = rest.data; console.log(notes)})
+
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+
+promise2.then(response => {console.log(response)})
+
+
